92fb601e0665dc88eeb8fc963c66606f71b9c646
cjsmith33 <charleswsmith8@gmail.com>
2021-07-21 08:26:54 -0700

    initial commit

---
+++ src/main/java/com/chucksmith/Application.java
@@ -0,0 +1,11 @@
+package com.chucksmith;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Table roundTable = new Table();
+        roundTable.setShape("round");
+        System.out.println(roundTable);
+    }
+}
